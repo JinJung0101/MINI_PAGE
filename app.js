56a0a8ef2fb3d57d842fec7d745252f45e1ca710
JinJung0101 <qwe5344641@gmail.com>
2023-12-28 21:29:12 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('MUV Main Page');
+});
+
+app.listen(3000, function() {
+    console.log("3000포트로 노드서버 오픈");
+});
